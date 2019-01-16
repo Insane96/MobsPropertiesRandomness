552315070c18474b153b4bb6ff2070394831bf97
Insane-96 <insane96mcp@gmail.com>
2019-01-16 11:53:36 +0100

    If debug is enabled show more in chat on world join

--- common/net/insane96mcp/mpr/events/PlayerLoggedIn.java
+++ common/net/insane96mcp/mpr/events/PlayerLoggedIn.java
@@ -3,6 +3,7 @@ package net.insane96mcp.mpr.events;
 import net.insane96mcp.mpr.MobsPropertiesRandomness;
 import net.insane96mcp.mpr.json.Group;
 import net.insane96mcp.mpr.json.Mob;
+import net.insane96mcp.mpr.lib.Properties;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,9 +14,14 @@ public class PlayerLoggedIn {
 
 	@SubscribeEvent
 	public static void EventPlayerLoggedIn(PlayerLoggedInEvent event) {
+		if (Properties.config.debug)
+			event.player.sendMessage(new TextComponentTranslation(MobsPropertiesRandomness.RESOURCE_PREFIX + "reload_json"));
 		boolean correctlyLoaded = Group.LoadGroups() &&	Mob.LoadJsons();
 		
-		if (!correctlyLoaded)
+		if (!correctlyLoaded) {
 			event.player.sendMessage(new TextComponentTranslation(MobsPropertiesRandomness.RESOURCE_PREFIX + "json_reload_error"));
+			if (Properties.config.debug)
+				event.player.sendMessage(new TextComponentTranslation(MobsPropertiesRandomness.RESOURCE_PREFIX + "json_count", String.valueOf(Mob.mobs.size())));
+		}
 	}
 }
