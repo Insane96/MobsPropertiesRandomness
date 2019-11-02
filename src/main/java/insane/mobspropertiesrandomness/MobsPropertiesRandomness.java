5631ef5a8ca824f5035cbfa6d0163e87f281756e
Insane-96 <insane96mcp@gmail.com>
2019-11-02 16:07:41 +0100

    Setup Empty Mod

---
+++ src/main/java/insane/mobspropertiesrandomness/MobsPropertiesRandomness.java
@@ -0,0 +1,40 @@
+package insane.mobspropertiesrandomness;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(MobsPropertiesRandomness.MOD_ID)
+public class MobsPropertiesRandomness
+{
+    public static final String MOD_ID = "mobspropertiesrandomness";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public MobsPropertiesRandomness() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+    }
+}
