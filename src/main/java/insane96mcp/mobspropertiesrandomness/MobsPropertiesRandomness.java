d87c9102a95bcf1ca819d1c3f231588fbe6429ef
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-08-06 13:32:46 +0200

    Changed JsonReloadListener to ReloadListener and actually registered it.

--- src/main/java/insane96mcp/mobspropertiesrandomness/MobsPropertiesRandomness.java
+++ src/main/java/insane96mcp/mobspropertiesrandomness/MobsPropertiesRandomness.java
@@ -1,6 +1,10 @@
 package insane96mcp.mobspropertiesrandomness;
 
+import insane96mcp.mobspropertiesrandomness.data.MPRReloadListener;
 import insane96mcp.mobspropertiesrandomness.setup.Config;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -22,6 +26,12 @@ public class MobsPropertiesRandomness
 
         ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.COMMON, Config.COMMON_SPEC, "MobsPropertiesRandomness/" + MOD_ID + ".toml");
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::preInit);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerAboutToStart(AddReloadListenerEvent event) {
+        event.addListener(MPRReloadListener.INSTANCE);
     }
 
     public void preInit(FMLCommonSetupEvent event) {
--- src/main/java/insane96mcp/mobspropertiesrandomness/data/MPRReloadListener.java
+++ src/main/java/insane96mcp/mobspropertiesrandomness/data/MPRReloadListener.java
@@ -1,33 +1,31 @@
 package insane96mcp.mobspropertiesrandomness.data;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
 import insane96mcp.mobspropertiesrandomness.MobsPropertiesRandomness;
 import insane96mcp.mobspropertiesrandomness.json.MPRMob;
 import insane96mcp.mobspropertiesrandomness.utils.FileUtils;
 import insane96mcp.mobspropertiesrandomness.utils.Logger;
-import net.minecraft.client.resources.JsonReloadListener;
+import net.minecraft.client.resources.ReloadListener;
 import net.minecraft.profiler.IProfiler;
 import net.minecraft.resources.IResourceManager;
-import net.minecraft.util.ResourceLocation;
 
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-public class MPRReloadListener extends JsonReloadListener {
+public class MPRReloadListener extends ReloadListener<Void> {
 	public static final List<MPRMob> MPR_MOBS = new ArrayList<>();
 
 	public static final MPRReloadListener INSTANCE;
-	public static final Gson GSON = new GsonBuilder()
-			.disableHtmlEscaping()
-			.create();
 
 	public MPRReloadListener() {
-		super(GSON, "mpr_json");
+		super();
+	}
+
+	@Override
+	protected Void prepare(IResourceManager iResourceManager, IProfiler iProfiler) {
+		return null;
 	}
 
 	static {
@@ -35,7 +33,7 @@ public class MPRReloadListener extends JsonReloadListener {
 	}
 
 	@Override
-	protected void apply(Map<ResourceLocation, JsonElement> resourceLocationJsonElementMap, IResourceManager iResourceManager, IProfiler iProfiler) {
+	protected void apply(Void objectIn, IResourceManager iResourceManager, IProfiler iProfiler) {
 		File jsonFolder = new File(MobsPropertiesRandomness.CONFIG_FOLDER + "/json");
 		if (!jsonFolder.exists())
 			jsonFolder.mkdir();
