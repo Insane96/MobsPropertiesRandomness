22e6c4f56ab246cce572c670363f017491c19c78
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-08-12 08:35:15 +0200

    MPRDifficultyModifier apply min and max now returns a MPRRange

--- src/main/java/insane96mcp/mobspropertiesrandomness/json/utils/difficulty/MPRDifficultyModifier.java
+++ src/main/java/insane96mcp/mobspropertiesrandomness/json/utils/difficulty/MPRDifficultyModifier.java
@@ -3,7 +3,7 @@ package insane96mcp.mobspropertiesrandomness.json.utils.difficulty;
 import com.google.gson.annotations.SerializedName;
 import insane96mcp.mobspropertiesrandomness.exception.InvalidJsonException;
 import insane96mcp.mobspropertiesrandomness.json.IMPRObject;
-import net.minecraft.util.Tuple;
+import insane96mcp.mobspropertiesrandomness.json.utils.MPRRange;
 import net.minecraft.world.Difficulty;
 
 import java.io.File;
@@ -24,12 +24,12 @@ public class MPRDifficultyModifier implements IMPRObject {
 			throw new InvalidJsonException("Difficulty Modifier is missing both difficulty and local_difficulty objects. " + this, file);
 	}
 
-	public Tuple<Float, Float> applyModifier(Difficulty worldDifficulty, float worldLocalDiff, float min, float max) {
+	public MPRRange applyModifier(Difficulty worldDifficulty, float worldLocalDiff, float min, float max) {
 		max = applyModifier(worldDifficulty, worldLocalDiff, max);
 		if (!affectsMaxOnly)
 			min = applyModifier(worldDifficulty, worldLocalDiff, min);
 
-		return new Tuple<>(min, max);
+		return new MPRRange(min, max);
 	}
 
 	public float applyModifier(Difficulty worldDifficulty, float worldLocalDiff, float value) {
