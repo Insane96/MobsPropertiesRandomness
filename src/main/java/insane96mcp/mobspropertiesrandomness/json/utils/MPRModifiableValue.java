c0864ebfe220c9151bef267b79e331780ed8a000
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-09-15 21:05:10 +0200

    Started working on custom deserializer but can't fallback to default deserializer

--- src/main/java/insane96mcp/mobspropertiesrandomness/json/utils/MPRModifiableValue.java
+++ src/main/java/insane96mcp/mobspropertiesrandomness/json/utils/MPRModifiableValue.java
@@ -8,8 +8,13 @@ import net.minecraft.world.World;
 
 import java.io.File;
 
+//@JsonAdapter(MPRModifiableValue.Deserializer.class)
 public class MPRModifiableValue extends MPRModifiable implements IMPRObject {
 	private Float value;
+
+	public MPRModifiableValue(float value) {
+		this.value = value;
+	}
 
 	public void validate(final File file) throws InvalidJsonException {
 		if (this.value == null)
@@ -34,4 +39,13 @@ public class MPRModifiableValue extends MPRModifiable implements IMPRObject {
 	public String toString() {
 		return String.format("ModifiableValue{value: %f, %s}", this.value, super.toString());
 	}
+
+	/*public static class Deserializer implements JsonDeserializer<MPRModifiableValue> {
+		@Override
+		public MPRModifiableValue deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+			if (typeOfT == float.class)
+				return new MPRModifiableValue(json.getAsFloat());
+			return context.deserialize(json, MPRModifiableValue.class);
+		}
+	}*/
 }
