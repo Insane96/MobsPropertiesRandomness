3fe16b923c8ab306762728f438edd048e08fe4fe
Alberto Del Villano <delvillano.alberto@gmail.com>
2021-09-15 21:50:39 +0200

    bonus and step are now required for eachother

--- src/main/java/insane96mcp/mobspropertiesrandomness/json/utils/modifier/MPRPosModifier.java
+++ src/main/java/insane96mcp/mobspropertiesrandomness/json/utils/modifier/MPRPosModifier.java
@@ -14,40 +14,38 @@ public class MPRPosModifier implements IMPRObject {
 	 Each 'distance_from_spawn_step' blocks from spawn will increase the value to modify by 'distance_from_spawn_bonus'%. The formula is 'bonus * (distance_from_spawn / step)'. E.g. with step = 100 and bonus = 0.02 when a mob spawns 150 blocks from spawn will have the value modified as '0.02 * (150 / 100)' = '0.02 * 1.5' = '+3%'
 	 */
 	@SerializedName("distance_from_spawn_bonus")
-	public float distanceFromSpawnBonus;
+	public Float distanceFromSpawnBonus;
 	@SerializedName("distance_from_spawn_step")
-	public float distanceFromSpawnStep;
+	public Float distanceFromSpawnStep;
 
 	/*
 	 Each 'depth_step' blocks below 'depth_starting_level' will increase the value to modify by 'depth_bonus'%. The formula is 'bonus * (MAX(starting_level - spawning_y, 0) / step)'. E.g. with step = 1, bonus = 0.01 and starting_level = 64 when a mob spawns at y = 24 you'll have '0.01 * (MIN(64 - 24, 0) / 1)' = '0.01 * 40' = '+40%'
 	 */
 	@SerializedName("depth_bonus")
-	public float depthBonus;
+	public Float depthBonus;
 	@SerializedName("depth_step")
-	public float depthStep;
+	public Float depthStep;
 	@SerializedName("depth_starting_level")
-	public float depthStartingLevel;
+	public Float depthStartingLevel;
 
 	/*
 	The two bonuses are summed up, so in the example the result would be a +43%
 	 */
 
 	public MPRPosModifier() {
-		this.distanceFromSpawnBonus = 0f;
-		//TODO Make these required if one of the two is present instead of giving a default value, except for depth starting level
-		this.distanceFromSpawnStep = 100f;
-		this.depthBonus = 0f;
-		this.depthStep = 1f;
 		this.depthStartingLevel = 64f;
 	}
 
 	@Override
 	public void validate(File file) throws InvalidJsonException {
+		if (this.distanceFromSpawnBonus != null && this.distanceFromSpawnStep == null || this.distanceFromSpawnBonus == null && this.distanceFromSpawnStep != null)
+			throw new InvalidJsonException("distance_from_spawn_bonus and distance_from_spawn_step are required for eachother. " + this, file);
 
+		if (this.depthBonus != null && this.depthStep == null || this.depthBonus == null && this.depthStep != null)
+			throw new InvalidJsonException("depth_bonus and depth_step are required for eachother. " + this, file);
 	}
 
 	public float applyModifier(World world, Vector3d pos, float value) {
-
 		//Distance from Spawn
 		Vector3d spawnPos = new Vector3d(world.getWorldInfo().getSpawnX(), world.getWorldInfo().getSpawnY(), world.getWorldInfo().getSpawnZ());
 		float distance = (float) spawnPos.distanceTo(pos);
